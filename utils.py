c639a992aaee809407031705ecf08958f55ebd3d
jdwiberg <jacobwiberg1133@gmail.com>
2026-01-15 22:10:41 -0500

    data json

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 def make_key(dir_pref: str, start: int, end: int):
@@ -43,12 +44,40 @@ def make_list(start: int, end: int):
                     with open(os.path.join(dir, "key2.json"), 'w') as f:
                         f.writelines(lines)
 
-for i in range(0, 100):
-    dir = "gold_standard/" + str(i)
-    for name in os.listdir(dir):
-        pth = os.path.join(dir, name)
-        if name == "key2.json":
-            os.rename(pth, os.path.join(dir, "key.json"))
+def make_full_json(start: int, end: int):
+    dir_pref = "gold_standard/"
+    for i in range(start, end+1):
+        dir = dir_pref + str(i)
+        for file in os.listdir(dir):
+            pth = os.path.join(dir, file)
+            text = []
+
+            pth_key = os.path.join(dir, "key.json")
+            pth_text = os.path.join(dir, f"{i}gs.txt")
+            with open(pth_key, "r") as k:
+                key = json.load(k)
+                with open(pth_text, "r") as t:
+                    text = "".join(t.readlines())
+                    with open(os.path.join(dir, "data.json"), 'w') as out:
+                        full = {
+                            "text": text,
+                            "key": key
+                        }
+                        json.dump(full, out, indent=4)
+                        out.close()
+                        t.close()
+                        k.close()
+            
+
+            
+
+def rename_all():
+    for i in range(0, 100):
+        dir = "gold_standard/" + str(i)
+        for name in os.listdir(dir):
+            pth = os.path.join(dir, name)
+            if name == "key2.json":
+                os.rename(pth, os.path.join(dir, "key.json"))
 
 
-
+make_full_json(0, 99)
